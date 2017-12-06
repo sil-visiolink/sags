33524d93198c9745329ca299da6118075cbe1dfb
Simon Lauridsen <sil@visiolink.com>
2017-12-06 13:06:11 +0100

    Removed temp background colors

--- sags/treatmentViewController.swift
+++ sags/treatmentViewController.swift
@@ -6,11 +6,30 @@
 //  Copyright © 2017 Kameli. All rights reserved.
 //
 
+//  Kingfisher Framework
+//
+//  Copyright (c) 2017 Wei Wang
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 import Foundation
 import UIKit
 import Kingfisher
 
-class treatmentCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class treatmentCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var treatmentHeader: UILabel!
     @IBOutlet weak var treatmentComment: UILabel!
@@ -23,7 +42,14 @@ class treatmentCollectionViewController: UIViewController, UICollectionViewDataS
         // Do any additional setup after loading the view, typically from a nib.
         
         treatmentHeader.text = self.treatment["behandlingstype"] as? String
-        treatmentComment.text = self.treatment["kommentar"] as? String
+        
+        let comment = self.treatment["kommentar"] as? String
+        if (comment?.isEmpty)! {
+            treatmentComment.text = "Ingen kommentarer"
+        } else {
+            treatmentComment.text = self.treatment["kommentar"] as? String
+        }
+        
         imageArray = treatment["billeder"] as! [Any]
     }
     
@@ -44,13 +70,14 @@ class treatmentCollectionViewController: UIViewController, UICollectionViewDataS
     
     func configureCell(cell: treatmentCollectionViewCell, forItemAtIndexPath: IndexPath) {
         let treatmentImageDict = imageArray[forItemAtIndexPath.item] as! [String : Any]
-        cell.commentLabel.text = treatmentImageDict["billedbem"] as? String
+//        cell.commentLabel.text = treatmentImageDict["billedbem"] as? String
+        cell.commentLabel.text = "HEST"
         
-        let url = URL(string: (treatmentImageDict["billedfil"] as? String)!)!
+        let url = URL(string: String(format: "http://sags.suvogaranti.dk/billeder/%@", (treatmentImageDict["billedfil"] as? String)!))
         cell.imageView.kf.setImage(with: url)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 300, height: 300)
+        return CGSize(width: self.view.frame.size.width, height: self.view.frame.size.width/1.33 + 40)
     }
 }
